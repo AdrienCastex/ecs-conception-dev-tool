b2a4770487cdd5bf4cfbd37c9d7b928e45ff8716
Adrien Castex <adrien.castex@protonmail.com>
2023-10-23 18:00:12 +0200

    add the systems column for future use

---
+++ src/app/view/SystemsColumn/SystemsColumnView.tsx
@@ -0,0 +1,12 @@
+import React, {  } from "react";
+import "./SystemsColumnStyle";
+
+export function SystemsColumnView({}: {}) {
+    return <div className="systems-column main-column">
+        <div className="column-header">
+            <div className="column-name">Systems</div>
+        </div>
+        <div className="column-body">
+        </div>
+    </div>
+}
